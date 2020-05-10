3cbe3411d7eb32a3432ce6424d9ffeb6b8d9d3a7
1YJY1 <1974368118@qq.com>
2020-05-10 23:27:17 +0800

    cloud function

--- cloudfunctions/getOrderList/index.js
+++ cloudfunctions/getOrderList/index.js
@@ -6,14 +6,14 @@ cloud.init()
 const db = cloud.database();
 const order = db.collection('orders')
 
-const MAX_LIMIT = 20;
+const MAX_LIMIT = 100;
 // 云函数入口函数
 exports.main = async (event, context) => {
   //获取集合中记录总数
   const countResult = await order.count();
   const total = countResult.total;
   //计算分几次取
-  const batchTimes = Math.ceil(total / 20);
+  const batchTimes = Math.ceil(total / MAX_LIMIT);
   const tasks = [];
   for (let i = 0; i < batchTimes; i++) {
     const promise = order.where({_openid:event.openid}).orderBy('time', 'desc').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get();
